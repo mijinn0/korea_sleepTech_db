cc568fd2a544020110366983064a4b29a27baf86
mijinn0 <pmj_0818@naver.com>
2025-04-30 17:49:10 +0900

    202504320 springboot 실습용DB 수정

--- springboot_db.sql
+++ springboot_db.sql
@@ -9,3 +9,24 @@ CREATE TABLE IF NOT EXISTS test (
 );
 
 SELECT * FROM test;
+
+-- student 테이블 --
+CREATE TABLE student (
+	id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE
+);
+
+SELECT * FROM student;
+
+-- book 테이블 --
+CREATE TABLE book (
+	id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    writer VARCHAR(50) NOT NULL,
+    title VARCHAR(100) NOT NULL,
+	content VARCHAR(500) NOT NULL,
+    category VARCHAR(255) NOT NULL,
+    CONSTRAINT chk_category CHECK (category IN ('NOVEL', 'ESSAY', 'POEM', 'MAGAZINE'))
+);
+
+SELECT * FROM book;
